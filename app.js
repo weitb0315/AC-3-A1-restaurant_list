d51fbd312623bb3d64fd747af206ed111121b24b
weitb0315 <weitb0315@gmail.com>
2023-07-18 21:29:29 +0800

    feat: modify detail page

--- app.js
+++ app.js
@@ -36,17 +36,21 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+// detail頁面路由設定，顯示特定餐廳
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('detail', { restaurant }))
+    .catch(error => console.error(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
     return (restaurant.name.toLowerCase().includes(keyword.toLowerCase())) || (restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
-})
-
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
 })
 
 app.listen(port, () => {
