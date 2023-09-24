0634ff05bce61e33e9580398e85266957443bdbf
weitb0315 <weitb0315@gmail.com>
2023-09-24 22:44:43 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -7,29 +7,31 @@ const restaurantList = require('./restaurant.json')
 const restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const usePassport = require('./config/passport')
+const methodOverride = require('method-override')
+const routes = require('./routes')
 
-usePassport(app)
+require('./config/mongoose')
+// 設定樣板引擎為handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
+usePassport(app)
 app.use(bodyParser.urlencoded({ extended: true }))
-// 載入method-override
-const methodOverride = require('method-override')
-// 引用路由器
-const routes = require('./routes')
-require('./config/mongoose')
 
 app.use(express.static('public'))
 // 設定每一筆請求都會透過methodOverride進行前置處理
 app.use(methodOverride('_method'))
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 // 將request導入路由器
 app.use(routes)
-
-// 設定樣板引擎為handlebars
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Express is listen on localhost:${port}`)
