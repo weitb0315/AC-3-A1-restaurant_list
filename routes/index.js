2f6085807d0db103a11fe3c6ca3d036962ffb8a5
weitb0315 <weitb0315@gmail.com>
2023-09-24 21:53:18 +0800

    feat: add user authentication in restaurant and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,17 @@
-// 引用Express與Express路由器
 const express = require('express')
 const router = express.Router()
-// 準備引入路由模組
-
-// 引入home模組程式碼
 const home = require('./modules/home')
-// 將網址結構符合/字串的request導向home
-router.use('/', home)
-
-// 引入restaurants模組程式碼
 const restaurants = require('./modules/restaurants')
-// 將網址結構符合/restaurants字串開頭的request導向restaurants模組
-router.use('/restaurants', restaurants)
-
 const search = require('./modules/search')
-router.use('/search', search)
-
-// 引入sort模組程式碼
 const sort = require('./modules/sort')
-router.use('/sort', sort)
-
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
+
+router.use('/restaurants', authenticator, restaurants)
+router.use('/search', search)
+router.use('/sort', sort)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 // 匯出路由器
 module.exports = router
